640e900619644a2c6b1807a0fd64d4fc4e874a9e
timofey <tim02timka@mail.ru>
2022-06-15 15:14:33 +0700

    finaly fix ci 10

--- src/config/ServerData.config.js
+++ src/config/ServerData.config.js
@@ -1,8 +1,8 @@
 
 class ServerData{
     constructor(){
-        if (process.env.NODE_ENV == "production" || process.env.REACT_APP_TEST == "true"){
-            this.host = 'http://45.133.218.11:5501' ;
+        if (process.env.REACT_APP_TEST == "true"){
+            this.host = 'http://localhost:5501' ;
         }else{
             this.host = 'http://45.133.218.11:5501' ;
         }
