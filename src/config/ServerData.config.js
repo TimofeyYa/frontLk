440898ff3536f071f362cb62db08e8c8aeb01c08
timofey <tim02timka@mail.ru>
2022-06-15 15:01:04 +0700

    finaly fix ci 8

--- src/config/ServerData.config.js
+++ src/config/ServerData.config.js
@@ -2,7 +2,7 @@
 class ServerData{
     constructor(){
         if (process.env.NODE_ENV == "production" || process.env.REACT_APP_TEST == "true"){
-            this.host = 'http://10.10.10.3:5501' ;
+            this.host = 'http://45.133.218.11:5501' ;
         }else{
             this.host = 'http://45.133.218.11:5501' ;
         }
